e5433386acdce540872a26cb2d1db7927ef6e1a1
K0V0 <kovo.mato@gmail.com>
2026-01-31 22:21:59 +0100

    modify imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { appStateDAO } from "./backend/repository/AppStateRepository";
 import { appStateInit } from "./backend/datasource/init/AppStateInit";
 
-import "./ui/components/workspace";
+import "./ui/components/workspaceComponent";
 
 (async () => {
     await appStateDAO.init(appStateInit)
