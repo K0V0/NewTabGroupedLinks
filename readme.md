53b34dc2e96ef73e70c94a4431bae12754106ca4
K0V0 <kovo.mato@gmail.com>
2026-02-01 02:36:14 +0100

    add docu and comments

---
+++ readme.md
@@ -0,0 +1,21 @@
+NewTabGroupedLinks
+==================
+
+*Google Chrome & Chrome based browsers extension to use new tab as start page with links.*
+<br>
+*Links are sorted into colums/blocks by categories (groups) in which they can occur independently
+or in sub-categories that can be expanded or folded by default by user preference.*
+<br>
+*User should be able to define multiple workspaces (environments) with links that he can switch into.*
+
+Build & Run
+-----------
+
+- install ```node``` version 20+
+- install Vite (for builds) and TypeScript
+  ```
+  npm init -y
+  npm install -D vite typescript
+  ```
+- ```npm run build``` will generate unpacked extension under ```/dist```
+
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,21 @@ import { appStateInit } from "./backend/datasource/init/AppStateInit";
 
 import {WorkspaceComponent} from "./ui/components/workspaceComponent";
 
+// define custom elements used BEFORE they are created/constructed
+// otherwise You are unable to call custom methods for example for
+// injecting repository later on them
 customElements.define("workspace-root", WorkspaceComponent);
 
 (async () => {
-    await appStateDAO.init(appStateInit);
-    // document.body.innerHTML = `<workspace-root></workspace-root>`;
-    console.log("main.ts bootstrap done ...");
 
-    const workspaceElement = document.createElement("workspace-root") as WorkspaceComponent;
+    // initialize "app data" object and repository, WAIT FOR IT !!!
+    await appStateDAO.init(appStateInit);
+
+    // create custom HTML element ONLY THAT WAY otherwise (innerHTML() method or
+    // raw in html file) construction of object happens before initialized "repository"
+    // is injected and program crashes
+    const workspaceElement = document
+        .createElement("workspace-root") as WorkspaceComponent;
     workspaceElement.setRepository(appStateDAO);
     document.body.appendChild(workspaceElement);
 
