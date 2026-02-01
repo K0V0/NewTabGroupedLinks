4bab1dd9b6221de61cdf2b1b7a9ba7ed7b08ecac
K0V0 <kovo.mato@gmail.com>
2026-02-01 13:22:18 +0100

    view model data modep POC

--- src/ui/components/workspaceComponent.ts
+++ src/ui/components/workspaceComponent.ts
@@ -1,5 +1,5 @@
 import {AppStateRepository} from "../../backend/repository/AppStateRepository";
-import {WorkspaceViewModel} from "../../viewModel/workspaceViewModel";
+import {WorkspaceViewModel, WorkspaceViewModelAttributes} from "../../viewModel/workspaceViewModel";
 
 export class WorkspaceComponent extends HTMLElement {
 
@@ -18,14 +18,16 @@ export class WorkspaceComponent extends HTMLElement {
 
     render() {
         console.log("Workspace component render");
-        this.workspaceViewModel.environmentName
-            .subscribe(environmentName => {
-                this.htmlTemplate(environmentName);
+        this.workspaceViewModel.attributesObservable
+            .subscribe(attrs => {
+                this.htmlTemplate(attrs);
             });
     }
 
-    private htmlTemplate(workspaceTitle: string): void {
-        this.innerHTML = `<h1>hello word ${workspaceTitle}</h1>`;
+    private htmlTemplate(attrs: WorkspaceViewModelAttributes): void {
+        this.innerHTML = `
+            <h1>${attrs.environmentName}</h1>
+        `;
     }
 
 }
--- src/viewModel/workspaceViewModel.ts
+++ src/viewModel/workspaceViewModel.ts
@@ -1,18 +1,50 @@
 import {AppStateRepository} from "../backend/repository/AppStateRepository";
 import {ObservableValue} from "../utils/observableValue";
+import {AppState, Environment} from "../backend/entity/AppStateEntity";
 
 export class WorkspaceViewModel {
 
-    readonly environmentName: ObservableValue<string> = new ObservableValue<string>("");
+    readonly attributesObservable: ObservableValue<WorkspaceViewModelAttributes>
+        = new ObservableValue<WorkspaceViewModelAttributes>(null as any);
+
+    private attributes: WorkspaceViewModelAttributes = new WorkspaceViewModelAttributes();
 
     constructor(repo: AppStateRepository) {
         repo.state$.subscribe(state => {
-
-            const envId = state.activeEnvironmentId;
-            const env = state.environments[envId];
-
-            this.environmentName.set(env?.name ?? "Unknown");
+            this.attributes.setState(state);
+            this.attributesObservable.set(this.attributes);
         });
     }
+}
 
+export class WorkspaceViewModelAttributes {
+
+    private state!: AppState;
+
+    private _environment!: Environment;
+    private _environmentName!: string;
+    private _environmentId!: string;
+
+    public setState(state: AppState) {
+        this.state = state;
+        this.updateAttributes();
+    }
+
+    private updateAttributes() {
+        this._environmentId = this.state.activeEnvironmentId;
+        this._environment = this.state.environments[this._environmentId];
+        this._environmentName = this._environment.name;
+    }
+
+    get environmentName(): string {
+        return this._environmentName;
+    }
+
+    get environmentId(): string {
+        return this._environmentId;
+    }
+
+    get environment(): Environment {
+        return this._environment;
+    }
 }
