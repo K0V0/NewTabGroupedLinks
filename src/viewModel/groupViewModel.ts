013c7d3a39b662d692ccdda8e0c625ce7ee5a4eb
kovo <kovo.mato@gmail.com>
2026-02-03 01:20:21 +0100

    fill up view data model for dispalying content of groups

--- src/viewModel/groupViewModel.ts
+++ src/viewModel/groupViewModel.ts
@@ -2,23 +2,23 @@ import {ObservableValue} from "../utils/observableValue";
 import {AppStateRepository} from "../backend/repository/AppStateRepository";
 import {Link, Subgroup} from "../backend/entity/AppStateEntity";
 
-export interface LinkDTO {
+export interface GroupItemDTO {
+    type: string;
+}
+
+export interface LinkDTO extends GroupItemDTO{
     id: string;
     title: string;
     url: string;
     position: number;
 }
 
-export interface SubgroupDTO {
+export interface SubgroupDTO extends GroupItemDTO {
     id: string;
     title: string;
     links: LinkDTO[];
     position: number;
 }
-
-export type GroupItemDTO =
-    | { type: "link"; data: Link }
-    | { type: "subgroup"; data: SubgroupDTO };
 
 export type GroupItemsDTO = GroupItemDTO[];
 
@@ -26,14 +26,40 @@ export class GroupViewModel {
 
     private isLinkInGroupAndEnv =
         (environmentId: string, groupId: string) =>
-            (link: Link | Subgroup): boolean =>
-                link.environmentId === environmentId &&
-                link.groupId === groupId;
+            (groupItem: Link | Subgroup): boolean =>
+                groupItem.environmentId === environmentId &&
+                groupItem.groupId === groupId;
 
     private isRootGroupLink =
         () =>
             (link: Link): boolean =>
-                link.subGroupId == null && link.subGroupId == ""
+                link.subGroupId === undefined || link.subGroupId === null || link.subGroupId === ""
+
+    private isSubGroup =
+        () =>
+            (subgroup: Subgroup): boolean =>
+                subgroup.url === undefined && subgroup.subGroupId === undefined
+
+    private isSubGroupLink =
+        () =>
+            (link: Link): boolean =>
+                link.subGroupId !== undefined && link.subGroupId !== null && link.subGroupId !== ""
+
+    private toLinkDTO = (link: Link): LinkDTO => ({
+        id: link.id,
+        title: link.title,
+        url: link.url,
+        position: link.position,
+        type: "link",
+    });
+
+    private toSubGroupDTO = (subgroup: Subgroup): SubgroupDTO => ({
+        id: subgroup.id,
+        title: subgroup.title,
+        links: [],
+        position: subgroup.position,
+        type: "subgroup"
+    });
 
     readonly groupItemsObservable: ObservableValue<GroupItemsDTO>
         = new ObservableValue(null as any);
@@ -51,37 +77,42 @@ export class GroupViewModel {
                 // console.log("-----------------------");
 
                 const currentEnvironmentId = attrs.activeEnvironmentId;
+                const links: Link[] = Object.values(attrs.links);
+                const subGroups: Subgroup[] = Object.values(attrs.subgroups);
 
-                // Links - any type
-                // const groupItems: (Link | Subgroup)[] = Object
-                //     .values(attrs.links)
-                //     .filter(this.isLinkInGroupAndEnv(currentEnvironmentId, groupId));
+                const result: GroupItemsDTO = [];
 
-                // sub-groups
-                // groupItems.push(
-                //     ...Object
-                //         .values(attrs.subgroups)
-                //         .filter(this.isLinkInGroupAndEnv(currentEnvironmentId, groupId)));
-
-                // Transform flat structure of separated links and subgroup into map-like structure
-
-                const groupItems: GroupItemsDTO = [];
-
-                // map "root group" links into viewModel structure
-                Object
-                    .values(attrs.links)
-                    .filter(this.isLinkInGroupAndEnv(currentEnvironmentId, groupId) && this.isRootGroupLink())
-                    .map(link => link.);
+                // fill "root group" links into viewModel structure
+                result.push(...links
+                    .filter((link: Link) =>
+                        this.isLinkInGroupAndEnv(currentEnvironmentId, groupId)(link)
+                        && this.isRootGroupLink()(link))
+                    .map(this.toLinkDTO));
 
                 // fill root groups into viewModel structure
+                result.push(...subGroups
+                    .filter((subgroup: Subgroup) => this.isSubGroup()(subgroup))
+                    .map(this.toSubGroupDTO));
 
-                // fill "subgroup" links into each groups
+                // fill "subgroup" links into each subgroups
+                result
+                    .filter((groupItem: GroupItemDTO) => groupItem.type === "subgroup")
+                    .forEach((subGroup: SubgroupDTO) => {
+                        subGroup.links.push(...links
+                            .filter((link: Link) => this.isSubGroupLink()(link))
+                            .filter((link: Link) => link.subGroupId === subGroup.id)
+                            .map(this.toLinkDTO));
+                    });
+
+                console.log("--------------------------------------");
+                console.log(result);
+                console.log("--------------------------------------");
 
                 // order 1st level items (root links, subgroups)
 
                 // order 2nd level items (links in each subgroup)
 
-                this.groupItemsObservable.set(groupItems);
+                this.groupItemsObservable.set(result);
             });
     }
 
